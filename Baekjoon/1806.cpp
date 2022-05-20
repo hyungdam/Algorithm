4f6a5fb96329434a03b81cd79873c8e1d5d48e12
HyungDam Shin <tlsgudeka@gmail.com>
2022-05-20 00:09:03 +0000

    clear

---
+++ Baekjoon/1806.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[100001];
+int N, S;
+
+int main() {
+	cin >> N >> S;
+	for(int i = 0; i < N; i++)
+		cin >> arr[i];
+	
+	int st = 0;
+	int end = 0;
+	int sum = arr[0];
+	int ans = N + 1;
+	
+}
